94c45d301e0a55e47488c3e8e29c4d3fa1ce33da
VanDyn <ajb6@hw.ac.uk>
2019-03-03 12:33:13 -0500

    Fully working and tested

--- ethereum/contracts/Market.sol
+++ ethereum/contracts/Market.sol
@@ -1,11 +1,6 @@
 pragma solidity ^0.4.22;
 contract Market {
 
-    struct Participant {
-        address agentAcc;
-        string name;
-    }
-
     struct Contractor {
       address contractAdd;
       address contractor;
@@ -13,46 +8,27 @@ contract Market {
       bool exists;
     }
 
-    Participant[] public Participants;
-    mapping(address=>address) publishedContracts;
-    mapping(address=>Contractor) awardedContracts;
-    mapping(address=>Contractor) deadContracts;
+    mapping(address=>address) public publishedContracts;
+    mapping(address=>Contractor) public awardedContracts;
+    mapping(address=>Contractor) public deadContracts;
 
     address public marketPublisher;
-    Contract private contracts;
-
 
     function Market() public {
         marketPublisher = msg.sender;
     }
 
-    function joinMarket(string _name) public {
-        //require statement here
-        Participant memory agent = Participant({
-            agentAcc: msg.sender,
-            name: _name
-        });
-        Participants.push(agent);
-    }
-
     function addContract(uint _x, uint _y, uint _z, uint _liveFor) public {     //Add a contract to the market for auction
-        address newContract = new Contract(_x, _y, _z, msg.sender,_liveFor);
+        require(publishedContracts[msg.sender]==0);
+        address newContract = new Contract(_x, _y, _z, msg.sender,_liveFor,this);
         publishedContracts[msg.sender] = newContract;
     }
 
     function getContractAddress(address _contractOwner) view public returns (address){
-      //Make sure agent has joined the market
-      bool isParticipant = false;
-      for(uint i=0; i < Participants.length; i++){
-          if(Participants[i].agentAcc == msg.sender){
-              isParticipant = true;
-          }
-      }
-      require(isParticipant);
       return publishedContracts[_contractOwner];
     }
 
-    function awardContract(address _to, address _contract, address _owner){
+    function awardContract(address _to, address _contract, address _owner) payable {
       require(publishedContracts[_owner]!=0);
 
       Contractor memory c = Contractor({
@@ -60,19 +36,21 @@ contract Market {
         contractor: _to,
         status: false,
         exists: true
+
       });
+
       awardedContracts[_owner]=c;
       delete(publishedContracts[_owner]);
     }
 
-    // function contractComplete() public {
-    //   require(awardedContracts[msg.sender].exists == true);
-    //   require(awardedContracts[msg.sender].status == false);
-    //
-    //   awardedContracts[msg.sender].status = true;
-    //   deadContracts[msg.sender] = awardedContracts[msg.sender];
-    //   delete(awardedContracts[msg.sender]);
-    // }
+    function contractComplete() public {
+      require(awardedContracts[msg.sender].exists == true);
+      require(awardedContracts[msg.sender].status == false);
+
+      awardedContracts[msg.sender].status = true;
+      deadContracts[msg.sender] = awardedContracts[msg.sender];
+      delete(awardedContracts[msg.sender]);
+    }
 }
 
 
@@ -94,15 +72,17 @@ contract Contract {
     bool public ended;
 
     address public Owner;
+    address public contractAdd;
+    address public marketAdd;
     mapping (address => contractStructure) private contracts;
 
     event newLowestBid(address bidder, uint amount);
     event contractAwarded(address winner, uint amount);
 
-    Market private market;
-
-    function Contract(uint _x, uint _y, uint _z, address _owner, uint _liveFor) public {
+    function Contract(uint _x, uint _y, uint _z, address _owner, uint _liveFor, address _marketAdd) public {
         Owner = _owner;
+        marketAdd = _marketAdd;
+        contractAdd = this;
         var liveContract = contracts[Owner];
         liveContract.x = _x;
         liveContract.y = _y;
@@ -140,11 +120,6 @@ contract Contract {
 
     }
 
-    //For TESTING purposes ONLY, remove before deployment!
-    function moveTimeForward(uint s) public {
-        endTime = endTime - s;
-    }
-
     function auctionEnd() public {
         //set this to only be called by owner
         //setup a minimum live time for contract
@@ -154,7 +129,8 @@ contract Contract {
 
         ended = true;
         emit contractAwarded(lowestBidder,lowestBid);
-        //market.awardContract(lowestBidder,this,Owner);
+        Market market = Market(marketAdd);
+        market.awardContract(lowestBidder,contractAdd,Owner);
     }
 
 }
--- test/Market.test.js
+++ test/Market.test.js
@@ -24,19 +24,9 @@ beforeEach(async () => {
   marketAddress = market.options.address;
 
 //anytime call function it is asyncronous operation so use await
-//Create two market patricpants human and drone with seperate accounts
-  await market.methods.joinMarket("Human").send({
-    from: accounts[0],
-    gas: '1000000'
-  });
 
-  await market.methods.joinMarket("Drone").send({
-    from: accounts[1],
-    gas: '1000000'
-  });
-
-//The human adds a contract to the market
-  await market.methods.addContract('1','2','3','60').send({
+//Account 0 adds a contract to the market
+  await market.methods.addContract('1','2','3','4').send({
     from: accounts[0],
     gas: '1000000'
   });
@@ -57,25 +47,11 @@ describe('Market', () => {
   it('deploys market', () => {
     assert.ok(market.options.address);
   });
-  //market participation test
-  it('allows participants to join the market', async () => {
-    const participant = await market.methods.Participants('0').call();
-    assert.equal(accounts[0], participant.agentAcc);
-  });
+
   //Throws an error if account not a participant in market place tries to get
   //contract
-  it('Only allows partcipants of market to retreive contract details', async () => {
-    let err = null;
-    //does not allow non participant to get contract
-    try {
-      await market.methods.getContractAddress(accounts[3]).send({
-        from: accounts[3],
-        gas: '1000000'
-      });
-    } catch (error) {
-      err = error;
-    }
-    assert.ok(err instanceof Error);
+  it('allows partcipants to retreive contract details', async () => {
+
     //Allows market particpant to get contract
     const conAdd = await market.methods.getContractAddress(accounts[0]).send({
       from: accounts[0],
@@ -83,6 +59,8 @@ describe('Market', () => {
     });
     assert.ok(conAdd);
   });
+
+  //Add tests for mappings
 
 });
 
@@ -145,19 +123,17 @@ describe('Contract', () => {
       }
       assert.ok(err instanceof Error);
 
-      //Required to pass test, remove and test will fail.
-      //Changes end time as no delay function for truffle.
-      //This demonstrates the require statement in auctionEnd works.
-      //Changing blocktime is too unpredictable.
-      await contract.methods.moveTimeForward('61').send({
-        from: accounts[0],
-        gas: '1000000'
+      await new Promise(resolve=> {
+        console.log('waiting 4 seconds');
+        setTimeout(resolve,4001);
       });
+
+
 
       await contract.methods.auctionEnd().send({
         from: accounts[0],
         gas: '1000000'
       });
       assert.equal(true, await contract.methods.ended().call());
-      });
+    }).timeout(5000);
 });
